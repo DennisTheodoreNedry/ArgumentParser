3f6670b11b807418cd57b7694bdf1ce5b5e13d80
--///--//-- <14398606+s9rA16Bf4@users.noreply.github.com>
2022-08-09 07:51:03 +0200

    Made the GO version of the argumentparser more modern

--- go/arguments/arguments.go
+++ go/arguments/arguments.go
@@ -12,42 +12,64 @@ type argument struct {
 	shortName         string   // Short command for this argument, i.e. '-h' instead of '--help'
 	desc              string   // Description of the command
 	argument_required bool     // Does this command require a value to function?
-	argument_value    string   // This will contain the passed value if something was passed
-	set               bool     // Has this argument been passed?
+	argument_value    string   // This will contain the passed value if something was passed [deprecated]
+	set               bool     // Has this argument been passed? [deprecated]
 	options           []string // Available options for this command
 }
 
 var defined_arguments []argument // This array will contain all the defined arguments
 
-func Argument_add(longName string, shortName string, argument_required bool, desc string, options []string) {
-	// Adds an argument
+/*
+*  Adds an argument but without any optional values for said argument
+**/
+func Argument_add(longName string, shortName string, argument_required bool, desc string) {
+	defined_arguments = append(defined_arguments, argument{longName, shortName, desc, argument_required, "NULL", false, []string{"NULL"}})
+}
+
+/*
+*  Adds an argument but with the possibility to use define optional values for said argument
+**/
+func Argument_add_with_options(longName string, shortName string, argument_required bool, desc string, options []string) {
 	defined_arguments = append(defined_arguments, argument{longName, shortName, desc, argument_required, "NULL", false, options})
 }
-func Argument_parse() {
-	// Parses all entered arguments, check if they contain the correct value and does all magic
+
+/*
+*  Parses all entered arguments, check if they contain the correct value and does all magic
+**/
+func Argument_parse() map[string]string {
+
+	toReturn := make(map[string]string) // Only the flags entered will be found here
+
 	for i := 1; i <= len(os.Args[1:]); i++ {
 		found := false
+		i_arg := os.Args[i] // The input argument
+
 		for j, def_arg := range defined_arguments {
-			if os.Args[i] == def_arg.longName || os.Args[i] == def_arg.shortName {
+			if i_arg == def_arg.longName || i_arg == def_arg.shortName {
 
-				if def_arg.argument_required && i+1 > len(os.Args[1:]) {
-					notify.Error("The argument "+os.Args[i]+" needs an argument to work!", "arguments.argument_parse()")
+				if def_arg.argument_required {
+					if i+1 > len(os.Args[1:]) {
+						notify.Error(fmt.Sprintf("The argument %s needs an argument to work!", "arguments.argument_parse()", i_arg))
+					}
 
-				} else if def_arg.argument_required {
 					possible_options := false
+					i_value := os.Args[i+1] // The input value
 
 					for _, opt := range def_arg.options {
-						if opt == os.Args[i+1] || opt == "NULL" {
-							defined_arguments[j].argument_value = os.Args[i+1] // Save the value
-							i += 1
+						if opt == i_value || opt == "NULL" {
+							defined_arguments[j].argument_value = i_value // Save the value (deprecated)
+							// I believe the j variable becomes totally deprecated with the new way of handling
+
+							toReturn[i_arg] = i_value
+
+							i += 1 // So that we skip one row
 							possible_options = true
 							break
 						}
-
 					}
 
 					if !possible_options {
-						notify_error_msg := "Unknown options " + os.Args[i+1] + " to argument " + os.Args[i] + ", possible options are ["
+						notify_error_msg := fmt.Sprintf("Unknown option %s to argument %s, possible options are [", i_value, i_arg)
 						for i, option := range def_arg.options {
 							notify_error_msg += option
 							if i+1 < len(def_arg.options) {
@@ -58,25 +80,31 @@ func Argument_parse() {
 
 						notify.Error(notify_error_msg, "arguments.argument_parse()")
 					}
+
 				}
 
-				defined_arguments[j].set = true
-				found = true
+				defined_arguments[j].set = true // (deprecated)
+				found = true                    // Indicate that we found the flag
 				break
 			}
 		}
+
 		if !found {
-			if os.Args[i] == "-h" || os.Args[i] == "--help" {
+			if i_arg == "-h" || i_arg == "--help" {
 				Argument_help()
 			} else {
-				notify.Error("The argument "+os.Args[i]+" was not defined!", "arguments.argument_parse()")
+				notify.Error(fmt.Sprintf("The argument %s was not defined!", i_arg), "arguments.argument_parse()")
 			}
 		}
+
 	}
+
+	return toReturn
 }
 
-func Argument_check(arg_name string) bool {
-	// Checks if a argument has been called
+// ----  DEPRECATED FUNCTIONS START ---- //
+func Argument_check(arg_name string) bool { // Checks if a argument has been called
+	notify.Warning("This function is deprecated and instead you should utilize the map being returned when parsing")
 	var toReturn bool = false
 	for _, arg := range defined_arguments {
 		if arg.longName == arg_name || arg.shortName == arg_name {
@@ -87,8 +115,8 @@ func Argument_check(arg_name string) bool {
 	return toReturn
 }
 
-func Argument_get(arg_name string) string {
-	// Gets the provided value for a set argument, and will return NULL otherwise
+func Argument_get(arg_name string) string { // Gets the provided value for a set argument, and will return NULL otherwise
+	notify.Warning("This function is deprecated and instead you should utilize the map being returned when parsing")
 	var toReturn string = "NULL"
 	for _, arg := range defined_arguments {
 		if (arg.longName == arg_name || arg.shortName == arg_name) && arg.set {
@@ -99,8 +127,12 @@ func Argument_get(arg_name string) string {
 	return toReturn
 }
 
+// ----  DEPRECATED FUNCTIONS END ---- //
+
+/*
+*  Prints all defined arguments
+**/
 func Argument_help() {
-	// Prints all defined arguments
 	fmt.Println("#### Definied Arguments ####")
 	for id := 0; id < len(defined_arguments); id++ {
 		value := defined_arguments[id].longName + ", " + defined_arguments[id].shortName
@@ -110,25 +142,19 @@ func Argument_help() {
 		}
 		value += " | " + defined_arguments[id].desc
 
-		if len(defined_arguments[id].options) > 0 { // There were some predefined options defined
+		if defined_arguments[id].options[0] != "NULL" { // There were some predefined options defined
 			options := " | options are ["
-			contains_options := false
 			for i, opt := range defined_arguments[id].options {
-
-				if opt != "NULL" {
-					options += opt
-					if i+1 < len(defined_arguments[id].options) {
-						options += ", "
-					}
-					contains_options = true
-
+				options += opt
+				if i+1 < len(defined_arguments[id].options) {
+					options += ", "
 				}
 			}
-			options += "]" // Removes the last piece which is a ',' and adds the last ']'
-			if contains_options {
-				value += options
-			}
+
+			options += "]"
+			value += options
 		}
+
 		fmt.Println(value)
 	}
 }
